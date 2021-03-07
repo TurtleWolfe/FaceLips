d27ff613c617550eb0b1c00d544c7ab1c159118a
TurtleWolfe <7327259+TurtleWolfe@users.noreply.github.com>
2021-03-06 22:28:31 -0500

    gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,71 @@
-import React from 'react';
+// import React, { useRef, useEffect } from "react";
+// import { drawMesh } from "./utilities";
+import React, { useRef, useEffect } from 'react';
 import logo from './logo.svg';
+import * as tf from "@tensorflow/tfjs";
+import * as facemesh from "@tensorflow-models/face-landmarks-detection";
+import Webcam from "react-webcam";
+import { drawMesh } from "./utilities";
+
 import './App.css';
+import { MediaPipeFaceMesh } from '@tensorflow-models/face-landmarks-detection/dist/types';
 
 function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+  // tf.setBackend('webgl'); // for webgl - this should be the default and is the one I'm using
+// If you are using the WebGL backend:
+require('@tensorflow/tfjs-backend-webgl');
+  //  Load posenet
+  const runFacemesh = async () => {
+    // OLD MODEL
+    // const net = await facemesh.load({
+    //   inputResolution: { width: 640, height: 480 },
+    //   scale: 0.8,
+    // });
+    // NEW MODEL
+    const net = await facemesh.load(facemesh.SupportedPackages.mediapipeFacemesh);
+    setInterval(() => {
+      detect(net);
+    }, 10);
+  };
+
+  //  Detect function
+  const detect = async (net: MediaPipeFaceMesh) => {
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // Get Video Properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // Set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // Set canvas width
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      // Make Detections
+      // OLD MODEL
+      //       const face = await net.estimateFaces(video);
+      // NEW MODEL
+      const face = await net.estimateFaces({input:video});
+      console.log(face);
+
+      // Get canvas context
+      const ctx = canvasRef.current.getContext("2d");
+      requestAnimationFrame(()=>{drawMesh(face, ctx)});
+      // drawMesh(face, ctx)
+    }
+  };
+
+  useEffect(()=>{runFacemesh()}, []);
+  // runFacemesh();
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +73,35 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Webcam
+          ref={webcamRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zIndex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zIndex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
       </header>
     </div>
   );
